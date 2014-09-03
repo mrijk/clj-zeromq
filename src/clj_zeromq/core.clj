bffe9c3d3afbbb16340c0913ca1b79b699837f5c
Maurits <maurits.rijk@gmail.com>
2014-09-03 21:51:29 +0200

    Initial commit

---
+++ src/clj_zeromq/core.clj
@@ -0,0 +1,50 @@
+(ns clj-zeromq.core)
+
+(defn- get-all-by-name
+  ""
+  [name]
+  (try 
+    (java.net.InetAddress/getAllByName name)
+    (catch java.net.UnknownHostException e 
+      (throw (IllegalArgumentException. e)))))
+
+(defn- inet6-address?
+  ""
+  [address]
+  (instance? java.net.Inet6Address address))
+
+(defn- find-first-inet-address
+  ""
+  [name ipv4only]
+  (first (filter #(not (and ipv4only (inet6-address? %))) 
+                 (get-all-by-name name))))
+
+(defn- resolve-name
+  ""
+  [name ipv4only]
+  (let [delimiter (.lastIndexOf name ":")]
+    (if (< delimiter 0)
+      (throw (IllegalArgumentException. name))
+      (let [addr (subs name 0 delimiter)
+            port (read-string (subs name (inc delimiter)))]
+        [port (find-first-inet-address addr ipv4only)]))))
+
+(defn create-tcp-address
+  ""
+  [address]
+  (let [[port addr] (resolve-name address false)]
+    (java.net.InetSocketAddress. addr port)))
+
+(defn get-port
+  ""
+  [address]
+  (.getPort address))
+
+(defn to-string
+  ""
+  [address]
+  (let [hostaddress (.getHostAddress (.getAddress address))
+        port (get-port address)]
+    (if (inet6-address? address)
+      (str "tcp://[" hostaddress "]:" port)
+      (str "tcp://" hostaddress ":" port))))
---
+++ test/clj_zeromq/core_test.clj
@@ -0,0 +1,27 @@
+(ns clj-zeromq.core-test
+  (:use clojure.test
+        clj-zeromq.core))
+
+(deftest valid-address
+  (testing "Valid address"
+    (is (instance? java.net.InetSocketAddress
+                   (create-tcp-address "gmail.com:8080")))))
+
+(deftest missing-port
+  (testing "Missing port"
+    (is (thrown? IllegalArgumentException (create-tcp-address "127.0.0.1")))))
+
+(deftest unknown-host
+  (testing "Unknown host"
+    (is (thrown? IllegalArgumentException 
+                 (create-tcp-address "maurits.rijk:8888")))))
+
+(deftest test-port
+  (testing "Port"
+    (let [address (create-tcp-address "gmail.com:8080")]
+      (is (= 8080 (get-port address))))))
+
+;(deftest to-string-ipv4
+;  (testing "String formatting"
+;    (let [address (create-tcp-address "gmail.com:8080")]
+;      (is (= "tcp://gmail.com:8080" (to-string address))))))
